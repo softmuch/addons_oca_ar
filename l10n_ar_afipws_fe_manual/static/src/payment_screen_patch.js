efd370d4cc34a2796ff56c39e5731feae68e02e2
yamil <yamilgiralda@gmail.com>
2026-06-07 16:56:55 +0200

    [UPD] l10n_ar_afipws_fe_manual

---
+++ l10n_ar_afipws_fe_manual/static/src/payment_screen_patch.js
@@ -0,0 +1,13 @@
+import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
+import { patch } from "@web/core/utils/patch";
+
+// l10n_ar_pos forces set_to_invoice(true) on every Argentine company order.
+// This patch reverts that so the invoice button is off by default.
+patch(PaymentScreen.prototype, {
+    onMounted() {
+        super.onMounted();
+        if (this.pos.isArgentineanCompany()) {
+            this.currentOrder.set_to_invoice(false);
+        }
+    },
+});
