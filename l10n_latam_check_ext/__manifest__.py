bc4b7f030e8a3acadac17126aba3bcd30b132f78
yamil <yamilgiralda@gmail.com>
2026-06-19 11:02:18 +0200

    [ADD] l10n_latam_check_ext

---
+++ l10n_latam_check_ext/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Extensión Cheques LATAM",
+    "version": "18.0.1.0.0",
+    "category": "Localization/Argentina",
+    "website": "https://github.com/OCA/l10n-argentina",
+    "author": "Odossey",
+    "license": "AGPL-3",
+    "summary": "Agrega tipo de cheque y fecha de emisión al modelo l10n_latam.check",
+    "depends": [
+        "l10n_latam_check",
+    ],
+    "data": [
+        "views/l10n_latam_check_ext_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
