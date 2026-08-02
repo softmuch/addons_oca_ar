b222574dbc641899b4e9575a2bd7454e18446110
yamil <yamilgiralda@gmail.com>
2026-08-02 22:39:19 +0200

    [UPD] Footer ARCA

---
+++ l10n_ar_afipws_fe_report/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Factura Electrónica AR - Footer ARCA",
+    "version": "19.0.1.0.0",
+    "category": "Accounting/Localizations",
+    "author": "yamil",
+    "license": "AGPL-3",
+    "summary": "Footer de comprobante autorizado (QR, CAE, leyenda ARCA) en el reporte de factura",
+    "depends": [
+        "l10n_ar",
+        "l10n_ar_afipws_fe",
+    ],
+    "data": [
+        "views/report_invoice.xml",
+    ],
+    "installable": True,
+}
